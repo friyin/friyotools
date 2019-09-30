35a32b0c4879c7e3abfc8309a2c88da135fa65d2
Daniel Fernández <daniel@dfer.io>
2019-09-30 19:36:29 +0000

    New -i flag to enter domain list, and -v to activate verbose mode (default quiet)
    Signed-off-by: Daniel Fernández <daniel@dfer.io>

--- buckeadora.py
+++ buckeadora.py
@@ -7,8 +7,18 @@ from botocore.exceptions import ClientError
 
 USAGE_HELP = """
 Usage:
-  %s -d domain
+  %s -i domain_list_file | -d one_domain [-q]
+  
+Use - as file for stdin
 """
+
+aws_key = None
+aws_session = None
+
+input_data = []
+
+verbose_mode = False
+
 
 class StringToSignParser(HTMLParser):
     def __init__(self):
@@ -41,16 +51,26 @@ class StringToSignParser(HTMLParser):
         return data_tok[1]
 
 
-def get_bucket_name(domain):
-    aws_key = aws_session.get_credentials().access_key
-    url = 'https://%s/?AWSAccessKeyId=%s&Expires=1766972005&Signature=a' % (domain, aws_key)
+def url_creator_from_input(domain_or_url_raw):
+    domain_or_url= domain_or_url_raw.strip()
+    detect_str = '?AWSAccessKeyId=%s&Expires=1766972005&Signature=a'
+    if domain_or_url.startswith("http://") or domain_or_url.startswith("https://"):
+        end_slash = '' if domain_or_url.endswith("/") else '/'
+        return domain_or_url + end_slash + (detect_str % aws_key)
 
-    print("Trying:", url)
+    return 'https://%s/?AWSAccessKeyId=%s&Expires=1766972005&Signature=a' % (domain_or_url, aws_key)
+
+
+def get_bucket_name(url):
+
+    if verbose_mode:
+        print("Trying:", url)
     r = requests.get(url, timeout=5)
     body = r.text
 
     if r.status_code != 403:
-        print("Not a bucket, exiting")
+        if verbose_mode:
+            print("Not a bucket, exiting")
         sys.exit(2)
 
     parser = StringToSignParser()
@@ -65,70 +85,98 @@ def check_bucket_permissions(bucket_name):
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(bucket_name)
 
-    print("Checking permissions on bucket", bucket_name)
+    if verbose_mode:
+        print("Checking permissions on bucket", bucket_name)
     try:
         for bucket_obj in bucket.objects.all():
-            print(" - Detected object:", bucket_obj.key)
+            if verbose_mode:
+                print(" - Detected object:", bucket_obj.key)
         print(" *** Bucket is READABLE!:", bucket_name)
     except ClientError:
-        print("not readable")
+        if verbose_mode:
+            print("not readable")
 
     try:
         bucket.put_object(Key='write-test.txt', Body='pwned!')
         bucket.delete_objects(Delete={'Objects': [{'Key': 'write-test.txt'}]})
     except ClientError:
-        print("not writable")
+        if verbose_mode:
+            print("not writable")
         return
 
     print(" *** Bucket is WRITABLE!:", bucket_name)
+
+
+def read_domains_from_input(path):
+    if path == "-":
+        infile = sys.stdin
+    else:
+        infile = open(path, "r")
+
+    result = infile.readlines()
+    infile.close()
+    return result
 
 
 def usage():
     print(USAGE_HELP % (sys.argv[0]))
 
 
+def process_data():
+    for domain_or_url in input_data:
+        try:
+            url = url_creator_from_input(domain_or_url)
+            bucket_name = get_bucket_name(url)
+            if not bucket_name:
+                if verbose_mode:
+                    print("No bucket detected, exiting")
+                sys.exit(1)
+
+            if verbose_mode:
+                print("Detected bucket:", bucket_name)
+            check_bucket_permissions(bucket_name)
+        except KeyboardInterrupt as ki:
+            raise ki
+        except:
+            if verbose_mode:
+                print("Can't connect to", domain_or_url)
+
+
 def main():
-    global aws_session
+    global aws_session, aws_key, input_data, verbose_mode
 
     try:
         aws_session = boto3.Session(profile_name='default')
+        aws_key = aws_session.get_credentials().access_key
     except botocore.exceptions.ProfileNotFound:
         print("Make sure AWS credentials are in ~/.aws")
         sys.exit(10)
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hd:k:", [])
+        opts, args = getopt.getopt(sys.argv[1:], "hd:i:v", [])
     except getopt.GetoptError as err:
         print(str(err))
         usage()
         sys.exit(2)
 
-    domain = None
     for o, a in opts:
         if o in ("-h", "--help"):
             usage()
             sys.exit(1)
         elif o == "-d":
-            domain = a
+            input_data = [a]
+        elif o == "-i":
+            input_data = read_domains_from_input(a)
+        elif o == "-v":
+            print("Verbose mode on")
+            verbose_mode = True
         else:
             assert False, "unhandled option"
 
-
-    if not domain:
+    if len(input_data) == 0:
         print("No domain specified")
+        usage()
         sys.exit(4)
-
-    try:
-        bucket_name = get_bucket_name(domain)
-        if not bucket_name:
-            print("No bucket detected, exiting")
-            sys.exit(1)
-
-        print("Detected bucket:", bucket_name)
-        check_bucket_permissions(bucket_name)
-    except:
-        print("Can't connect to domain", domain)
-
 
 if __name__ == '__main__':
     main()
